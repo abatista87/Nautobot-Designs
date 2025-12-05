c4859645bb22fa0c7ef68c99d67041ab39e9775d
abatista87 <64155753+abatista87@users.noreply.github.com>
2025-12-05 01:23:12 -0800

    Create context.py

---
+++ jobs/context.py
@@ -0,0 +1,11 @@
+# demo_designs_testing/jobs/nexus_vpc/context.py
+
+from nautobot_design_builder.context import DesignContext
+
+
+class NexusVpcContext(DesignContext):
+    """
+    Minimal context for the Nexus VPC Core design.
+    For now we just inherit from DesignContext and don't add anything custom.
+    """
+    pass
